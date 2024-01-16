47b7de681fa0fd4eaeaeea5a5aaf8d1c54abe412
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2024-01-16 12:27:49 +0200

    add index.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import './index.css';
 import store, { persistedStore } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
